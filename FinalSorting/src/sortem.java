2a273420b1ccc3c30032134d6f6e465e711d7da6
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2018-02-13 09:23:22 -0500

    aaa

--- FinalSorting/src/sortem.java
+++ FinalSorting/src/sortem.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
 
 public class sortem 
 {
 
 	public static void main(String[] args) 
 	{
-
+		String [] poo = {"poo", "loo"};
+		String [] loo = {"shoo", "roo"};
 	}
 
 	public static String [] mergeSort(String [] list) 
 	{
+		if(list.length == 1)
+		{
+		return list;
+		}
 		
+		String [] l = Arrays.copyOfRange(list,0,list.length/2);
+		String [] r = Arrays.copyOfRange(list,list.length/2, list.length);
+	
+		return(merge(mergeSort(l),mergeSort(r)));
 	}
 	
-	public static int partition(int [] list, int front, int back)
+//	public static int partition(int [] list, int front, int back)
 	{
 		
 	}
@@ -21,4 +31,38 @@ public class sortem
 	{
 		
 	}
+	
+	//helpers
+	public static String[] merge(String [] list1, String [] list2) 
+	{
+		//Precondition : Both lists are sorted
+		//Takes two arrays and merges them in ascending order
+	
+		String [] full = new String [(list1.length) + (list2.length)-1]; //i made it work but am confused about why its minus 1 and not 2
+		
+		int j = 0;
+		int l = 0;
+		int r = 0;
+		
+		while(l < list1.length && r < list2.length && j < full.length) 
+		{
+			if (list1[l].compareTo(list2[r]) >= 0)
+			{
+				full[j] = list2[r];
+				j++;
+				r++;
+			}			
+			
+			else
+			{
+				full[j] = list1[l];
+				j++;
+				l++;
+			}
+		}
+	
+		return full;
+	}
+	
+	
 }
