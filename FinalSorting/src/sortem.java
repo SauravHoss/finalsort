2f97e9659afdfdad68cb4a7c49a0f8a3f08a589d
Rome <Rome@Rome>
2018-02-12 20:42:33 -0500

    a

---
+++ FinalSorting/src/sortem.java
@@ -0,0 +1,24 @@
+
+public class sortem 
+{
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+	public static String [] mergeSort(String [] list) 
+	{
+		
+	}
+	
+	public static int partition(int [] list, int front, int back)
+	{
+		
+	}
+	
+	public static void quickSort(int [] list1, int front, int back) 
+	{
+		
+	}
+}
